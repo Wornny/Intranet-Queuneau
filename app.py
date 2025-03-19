6539cc36cdcb90013f21cfccff351181f702d90a
Léo FOULOGNE <foulognl@bts-sn.lrq>
2025-03-19 11:45:51 +0100

    Debut project

---
+++ app.py
@@ -0,0 +1,45 @@
+import csv
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/remplacement', methods=['GET', 'POST'])
+def remplacement():
+    if request.method == 'POST': 
+       
+        créneau = request.form['Créneau']
+        classe = request.form['classe']
+        matière = request.form['Matière']
+
+        
+        new_message = [{'Créneau': créneau, 'Classe': classe, 'Matière': matière}]
+        
+       
+        with open('remplacement.csv', mode='a', newline='', encoding='utf-8') as csvfile:
+            fieldnames = new_message[0].keys()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            r
+            csvfile.seek(0, 2)  
+            if csvfile.tell() == 0:
+                writer.writeheader()
+
+            writer.writerows(new_message)
+
+        
+        return redirect(url_for('confirmation', créneau=créneau, classe=classe, matière=matière))
+
+   
+    return render_template('remplacement.html')  
+
+@app.route('/confirmation')
+def confirmation():
+    
+    créneau = request.args.get('créneau')
+    classe = request.args.get('classe')
+    matière = request.args.get('matière')
+
+    return render_template('confirmation.html', créneau=créneau, classe=classe, matière=matière)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Projet d'intranet Queneau
+## Présentation du projet
+Objectif de ce site est de proposer au personnel du lycée de saisir des remplacements ou des déplacement. Il faudra pour cela se loger avec un compte individuel.
+
+## Fonctionnalités
+### Personnel lycée : 
+CR : Se loger (saisir login, se déloger, changer avatar, nouveau mot de passe ...)
+RDG: Visualiser la page d'accueil avec choix des pages à ouvrir
+JM : Afficher le tableau de remplacement
+LF : Soumettre un remplacement (formulaire)
+MLL : Soumettre un déplacement (formulaire)
+
+### Etudiant :
+ME : Visualiser le menu
+ME : Proposition de repas (formulaire)
+ 
+ ### Gestionnaire : 
+Placer des remplacements dans le tableau
+LF: Visualiser les remplacements proposés
+MLL : Visualiser les déplacements soumis
+ME : Visualisation des propositions de repas
+
+
+
+
+
+
