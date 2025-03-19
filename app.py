2704fc074db412a5efe24413c82f4ff9b21aa28a
pcpei <ruettecoco@gmail.com>
2025-03-19 10:57:50 +0100

    Ajouter de nouveau style + page html changer mot de passe

--- app.py
+++ app.py
@@ -17,6 +17,14 @@ def lire_utilisateurs():
                 utilisateurs[row[0]] = row[1]
     return utilisateurs
 
+# Sauvegarder les utilisateurs après modification
+def sauvegarder_utilisateurs(utilisateurs):
+    with open(CSV_FILE, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['login', 'password'])
+        for login, password in utilisateurs.items():
+            writer.writerow([login, password])
+
 @app.route('/', methods=['GET', 'POST'])
 def bonjour_post():
     return render_template('index.html')
@@ -30,11 +38,36 @@ def login():
 
         if login in utilisateurs and utilisateurs[login] == password:
             session['login'] = login
-            return "Connexion réussie"
+
+            # Si l'utilisateur a encore le mot de passe par défaut, on lui demande de le changer
+            if utilisateurs[login] == "azerty*123":
+                return redirect(url_for('changer_mot_de_passe'))
+            else:
+                return "Connexion réussie"
+
         else:
             return "Identifiants incorrects", 401
+
     return render_template('login.html')
 
+@app.route('/changer_mot_de_passe', methods=['GET', 'POST'])
+def changer_mot_de_passe():
+    if 'login' not in session:
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        nouveau_mdp = request.form['nouveau_mdp']
+        login = session['login']
+        
+        utilisateurs = lire_utilisateurs()
+        utilisateurs[login] = nouveau_mdp  # Mettre à jour le mot de passe de l'utilisateur
+        
+        # Sauvegarder les modifications dans le fichier CSV
+        sauvegarder_utilisateurs(utilisateurs)
+
+        return render_template("login.html")
+
+    return render_template('changer_mot_de_passe.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
