e540e1abcff0da663b9f7aa22679f5efca413ffc
foulognl <leo.foulogne@gmail.com>
2025-03-20 14:00:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-# REMPLACEMENT 
-
-
-
-
 import csv
 from flask import Flask, render_template, redirect, url_for, request
 
@@ -15,9 +10,9 @@ def remplacement():
         créneau = request.form['Créneau']
         classe = request.form['classe']
         matière = request.form['Matière']
-
+        mail_personnel = request.form ['Mail_personnel']
         
-        new_message = [{'Créneau': créneau, 'Classe': classe, 'Matière': matière}]
+        new_message = [{'Créneau': créneau, 'Classe': classe, 'Matière': matière, 'mail_personnel': mail_personnel}]
         
        
         with open('remplacement.csv', mode='a', newline='', encoding='utf-8') as csvfile:
@@ -32,7 +27,7 @@ def remplacement():
             writer.writerows(new_message)
 
         
-        return redirect(url_for('confirmation', créneau=créneau, classe=classe, matière=matière))
+        return redirect(url_for('confirmation', créneau=créneau, classe=classe, matière=matière, mail_personnel=mail_personnel))
 
    
     return render_template('remplacement.html')  
@@ -43,8 +38,9 @@ def confirmation():
     créneau = request.args.get('créneau')
     classe = request.args.get('classe')
     matière = request.args.get('matière')
+    mail_personnel = request.args.get('mail')
 
-    return render_template('confirmation.html', créneau=créneau, classe=classe, matière=matière)
+    return render_template('confirmation.html', créneau=créneau, classe=classe, matière=matière, mail=mail_personnel)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
