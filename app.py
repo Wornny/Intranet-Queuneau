cf2f92079c560c1609125c9e1a2315be3952993e
pcpei <ruettecoco@gmail.com>
2025-03-20 13:42:41 +0100

    ajout email connexion

--- app.py
+++ app.py
@@ -161,5 +161,51 @@ def ajouter_remplacement():
 
     return redirect('/remplacement')
 
+@app.route('/deplacement', methods=['GET', 'POST'])
+def deplacement_post():
+
+    return render_template('deplacement.html',)
+
+#Route pour l'affichage du formulaire (GET) ou le traitement du formulaire (POST)
+#Avec la méthode POST les données du formulaire sont extraites et ensuite sauvegardées
+#dans un fichier CSV. le formulaire est ensuite affiché
+@app.route('/save', methods=['POST'])
+def save_message():
+    address = request.form.get('address')
+    address_2 = request.form.get('address_2')  # Champ honeypot
+    postal_code = request.form.get('postal_code')
+    start_city = request.form.get('start_city')
+    car = request.form.get('car')
+    objet = request.form.get('objet')
+    end_city = request.form.get('end_city')
+    bussiness = request.form.get('bussiness')
+    date = request.form.get('date')
+    start_hours_go = request.form.get('start_hours_go')
+    end_hours_go = request.form.get('end_hours_go')
+    start_hours_back = request.form.get('start_hours_back')
+    end_hours_back = request.form.get('end_hours_back')
+
+    new_message = [{'address': address, 
+                    "address_2": address_2, 
+                    "postal_code": postal_code, 
+                    "start_city": start_city, 
+                    "car": car, 
+                    "objet" : objet, 
+                    "end_city" : end_city, 
+                    "bussiness" : bussiness,
+                    "date" : date,
+                    "start_hours_go" : start_hours_go,
+                    "end_hours_go" : end_hours_go,
+                    "start_hours_back" : start_hours_back,
+                    "end_hours_back" : end_hours_back
+                    }]
+
+    # Sauvegarde dans le fichier CSV
+    with open('message.csv', mode='a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=new_message[0].keys())
+        writer.writerows(new_message)
+
+    return redirect('/')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
